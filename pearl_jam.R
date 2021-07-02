55cb57c8edb5af98f9a8d7d1a0b1c09ac3d63724
Arthur Thina <arthur.thina@gmail.com>
2021-07-02 00:05:40 -0300

    add plot

--- pearl_jam.R
+++ pearl_jam.R
@@ -1,15 +1,36 @@
 library(tidyverse)
 library(lubridate)
+library(ggthemr)
 
-df_spot <- read_csv("/mnt/sda5/git/spotify/data/tracks.csv")
+df_spot <- read_csv("data/tracks.csv")
 
 str(df_spot)
 
  df_spot <- df_spot %>% 
   mutate(duration_ms = hms::as_hms(duration_ms / 1000))
- 
-pj_songs <- df_spot %>% filter(str_detect(artists, pattern = "Pearl Jam") & !str_detect(artists, pattern = "Hill")) %>% 
-  arrange(desc(energy))
 
-ggplot(pj_songs, aes(x = energy)) +
-  geom_boxplot()
+ 
+pj_songs <- df_spot %>% filter(str_detect(artists, pattern = "Pearl Jam") &
+                              !str_detect(artists, pattern = "Hill")) %>% 
+  top_n(10, energy)
+
+str(pj_songs)
+
+ggthemr("earth", type = "outer", layout = "plain", spacing = 2)
+ggplot(pj_songs, aes(x = energy, y = reorder(name, energy), fill = release_date)) +
+  geom_col() +
+  coord_cartesian(xlim = c(0.920, 1)) +
+  labs( title = "Top 10 Músicas mais 'Energéticas' do Pearl Jam",
+        subtitle = "De acordo com o Spotify",
+        caption = "Dados extraídos em 2021-07-02",
+        fill = "Data de Lançamento",
+        x = "Energia", 
+        y = "Nome da Música") +
+  theme(plot.title = element_text(size = 20, face = "bold"),
+        plot.subtitle = element_text(size = 15),
+        plot.caption = element_text(hjust = 1),
+        axis.text = element_text(size = 12),
+        axis.title.y = element_text(size = 16, face = "bold"),
+        axis.title.x = element_text(size = 16, face = "bold"))
+
+  
