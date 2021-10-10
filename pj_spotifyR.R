6064cede9b9a12608ac4b347f4b3389a45cee829
Arthur Thina <arthur.thina@gmail.com>
2021-10-10 02:23:49 -0300

    final energy

--- pj_spotifyR.R
+++ pj_spotifyR.R
@@ -1,17 +1,27 @@
 library(spotifyr)
 library(tidyverse)
+ggthemr::ggthemr("earth", type = "outer", layout = "plain", spacing = 2)
 
 Sys.getenv("SPOTIFY_CLIENT_ID")
 Sys.getenv("SPOTIFY_CLIENT_SECRET")
 access_token <- get_spotify_access_token()
 
 pearl_jam <- get_artist_audio_features('Pearl Jam') %>% 
-  distinct(track_name, .keep_all = TRUE)
+  distinct(track_name, .keep_all = TRUE) 
 
 glimpse(pearl_jam)
 
 pearl_jam$album_release_date <- anytime::anytime(pearl_jam$album_release_date)
 
-pearl_jam %>% 
-  count(key_name, sort = TRUE) 
+pj_album <- pearl_jam %>% 
+  group_by(album_name, album_release_date) %>% 
+  select("Album" = album_name, energy, "Release" = album_release_date) %>% 
+  summarise(Energy = mean(energy), .groups = "drop") %>% 
+  arrange(desc(Energy))
+
+ggplot(pj_album, aes(x = Energy, y = reorder(Album, Energy))) +
+  geom_col() +
+  coord_cartesian(xlim = c(.5, 1)) +
+  labs(x = NULL,
+       y = NULL)
 
