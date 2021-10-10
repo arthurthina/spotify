58f285f65158eeb832141056dd486527a77bd739
Arthur Thina <arthur.thina@gmail.com>
2021-10-10 02:58:58 -0300

    fix plot

--- pj_spotifyR.R
+++ pj_spotifyR.R
@@ -12,6 +12,7 @@ pearl_jam <- get_artist_audio_features('Pearl Jam') %>%
 glimpse(pearl_jam)
 
 pearl_jam$album_release_date <- anytime::anytime(pearl_jam$album_release_date)
+pearl_jam$album_name <- as.factor(pearl_jam$album_name)
 
 pj_album <- pearl_jam %>% 
   group_by(album_name, album_release_date) %>% 
@@ -22,6 +23,9 @@ pj_album <- pearl_jam %>%
 ggplot(pj_album, aes(x = Energy, y = reorder(Album, Energy))) +
   geom_col() +
   coord_cartesian(xlim = c(.5, 1)) +
+  geom_text(aes(label = Album), vjust = 0.4, hjust = 1.06,nudge_x = 0, color = "black", size = 4.5) +
   labs(x = NULL,
-       y = NULL)
-
+       y = NULL) +
+  theme(axis.title.y = element_blank(),
+        axis.text.y = element_blank(),
+        axis.ticks.y = element_blank())
